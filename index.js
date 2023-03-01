1520294056662c684d6e925bc3ede4dc0a398791
gyu0ne <124022652+gyu0ne@users.noreply.github.com>
2023-03-01 17:37:40 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+function test(str) {
+    alert(str);
+}
+
+function intro() {
+    window.scrollTo({
+        top: 180,
+        behavior: 'smooth'
+    })
+}
+
+function server() {
+    window.scrollTo({
+        top: 400,
+        behavior: 'smooth'
+    })
+}
